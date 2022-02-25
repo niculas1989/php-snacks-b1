fe336020577c45e221e287a6f398fcefbcdee32b
niculas1989 <nicolas.maranzano@libero.it>
2022-02-25 16:39:02 +0100

    added function for random numbers

--- snack-3.php
+++ snack-3.php
@@ -1,6 +1,9 @@
-<?php 
+<?php
 
 /* 
-Snack 3 <br />//! 3
-Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta.<br />
+Snack 3 //! 3
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta.
 */
+
+//| Random Number
+$randoms = rand(1, 15);
