7c77e8c0e171d9573ca3108169ec2f8fedce0111
niculas1989 <nicolas.maranzano@libero.it>
2022-02-25 14:50:41 +0100

    added first steps - snack-1

--- snack-1.php
+++ snack-1.php
@@ -2,7 +2,14 @@
 
 /*
 
-Snack 1 <br />//! 1
-Passare <br />come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”<br />
+Snack 1 //! 1
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 
 */
+
+//# prendiamo, attraverso il parametro GET, i seguenti valori: name, mail e age
+$name = $_GET['name'];
+
+$mail = $_GET['mail'];
+
+$age = $_GET['age'];
