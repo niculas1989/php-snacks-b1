968fb5cdc463593ebe396f113d302df8b886ea8e
niculas1989 <nicolas.maranzano@libero.it>
2022-02-25 14:40:05 +0100

    first commit

---
+++ snacks.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+
+Oggi ci eserciteremo con alcuni snack per prendere confidenza con la nuova sintassi di PHP e rinfrescare le basi di logica! Vi chiediamo di fare 3 snack ma ne lasciamo altri 2 come bonus
+
+Snack 1 //! 1
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+Snack 2 //! 2
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
+Snack 3 //! 3
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta.
+BONUS:
+Snack 4 //! 4
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi in base ai punti: ogni punto un nuovo paragrafo.
+Snack 5 //! 5
+Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
+Consigli:
+Non gettiamoci subito sul codice e facciamo prima un po' di ragionamento sul task e sulla struttura che vogliamo dare ai nostri dati.
+
+*/
