dc97f249d741b30965f4ece14ecc93e88d2c3152
niculas1989 <nicolas.maranzano@libero.it>
2022-02-25 16:45:16 +0100

    added first try for snack-3 - not working

--- snack-3.php
+++ snack-3.php
@@ -5,5 +5,18 @@ Snack 3 //! 3
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta.
 */
 
+//| Max numbers in arra
+$max_numbers = 15;
+
 //| Random Number
 $randoms = rand(1, 100);
+
+//| definiamo un array vuoto
+$array = [];
+var_dump($array);
+
+for ($i = 0; $i < $max_numbers; $i++) {
+    $array .= $randoms;
+}
+
+var_dump($array);
