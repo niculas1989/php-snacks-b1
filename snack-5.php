5e4529ad7770e73e8d70cc26465ac3e708476b48
niculas1989 <nicolas.maranzano@libero.it>
2022-02-26 11:32:30 +0100

    added new snake - snack-5.php

---
+++ snack-5.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Snack 5 <br />//! 5
+Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.<br />
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
