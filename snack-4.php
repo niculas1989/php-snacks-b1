eaa15ce01d305b472f65aad18d9b05ea208b2be8
niculas1989 <nicolas.maranzano@libero.it>
2022-02-26 12:22:13 +0100

    added comment on snack-4

--- snack-4.php
+++ snack-4.php
@@ -6,6 +6,8 @@ Prendere il paragrafo e suddividerlo in tanti paragrafi in base ai punti: ogni p
 
 $paragraph = 'Gli sfortunati sono soltanto un metro di riferimento. per i fortunati, tu sei sfortunato,. così io so di non esserlo, sfortunatamente. i fortunati riconoscono la fortuna solo quando la perdono. Prendi te stesso per esempio;. ieri stavi meglio di oggi, eppure ti ci voleva oggi per capirlo. Ma oggi è arrivato e ora è tardi. Hai visto?';
 
+//# cit. Slevin Patto Criminale
+
 $exploded = explode('.', $paragraph);
 
 var_dump($exploded);
